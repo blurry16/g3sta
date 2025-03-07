6b6d06c1556889d5674540ad002bcf084efc2a55
blurry16 <roma.reznik@gmail.com>
2025-03-07 23:17:53 +0300

    Update randomize.py

--- randomize.py
+++ randomize.py
@@ -4,7 +4,8 @@ from g3sta import G3STA_PATH
 
 if __name__ == '__main__':
     while True:
-        if input("continue? y/n ").strip().lower() == "n":
-            exit()
         with open(G3STA_PATH, "r", encoding="utf-8") as f:
             print(random.choice(f.read().split("\n")))
+        if input("continue? y/n ").strip().lower() == "n":
+            exit()
+        
